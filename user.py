7eba071d361f6c06bd5aeb044104bd08e6dad9a9
jek <zumazaki@gmail.com>
2023-01-24 21:52:54 +0700

    add user script

---
+++ user.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from datetime import datetime
+from dateutil import relativedelta
+from plan import Plan
+
+@dataclass
+class User:
+  username: str
+  plan: Plan
+  start_subs_time: datetime
+  referral_code: str=None
+
+  def __post_init__(self):
+    self.invoice = self.plan.price
+
+  def upgrade_plan(self, new_plan):
+
+    if new_plan.level <= self.plan.level:
+      print("Plan tidak boleh turun atau sama!")
+      return
+    
+    discount = 1  # 100%
+    difference = relativedelta.relativedelta(datetime.now(), self.start_subs_time)
+    if difference.years > 1:
+      discount = 0.95  # 95%
+
+
+    self.plan = new_plan
+    self.start_subs_time = datetime.now()
+    self.invoice = self.plan.price * discount
